4d03f28612c49f62fd011cf787dd275a1a9f43fa
kunxin-chor <chorkunxin@yahoo.com>
2021-06-25 08:12:16 +0000

    Added form module

---
+++ forms/index.js
@@ -0,0 +1,58 @@
+const forms = require('forms')
+
+// create some shortcuts
+const fields = forms.fields;
+const validators = forms.validators;
+
+var bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+const createProductForm = () => {
+    return forms.create({
+        // <label class="form-label">Name</label>
+        // <input type="text" name="name" />
+        'name': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'cost': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        })
+    })
+}
+
+module.exports = {
+    createProductForm,
+    bootstrapField
+}
