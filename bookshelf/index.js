27a50276cf310efe2b1e14d555170bf6d1c3e570
kunxin-chor <chorkunxin@yahoo.com>
2021-07-28 06:27:12 +0000

    Added postges

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -5,7 +5,10 @@ const knex = setupKnex({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST
+        host: process.env.DB_HOST,
+        'ssl': {
+            'rejectUnauthorized': true
+        }
     }
 })
 
